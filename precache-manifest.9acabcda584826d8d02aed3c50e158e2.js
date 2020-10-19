10a7081a106d1b5e605e5181246c781bc88d46cf
RaidenDilan <raiden18@me.com>
2020-10-19 22:26:29 +0100

    Updates

---
+++ precache-manifest.9acabcda584826d8d02aed3c50e158e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3316b78c4b7daac630a0",
+    "url": "/robofriends/static/css/main.8e7ef5f3.chunk.css"
+  },
+  {
+    "revision": "3316b78c4b7daac630a0",
+    "url": "/robofriends/static/js/main.f4965890.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "96c59059f513e63adff1",
+    "url": "/robofriends/static/css/2.9f3a93c0.chunk.css"
+  },
+  {
+    "revision": "96c59059f513e63adff1",
+    "url": "/robofriends/static/js/2.cdd0c6b0.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "af5bc7fd43be39a2a5635b7434acf2f2",
+    "url": "/robofriends/index.html"
+  }
+];
